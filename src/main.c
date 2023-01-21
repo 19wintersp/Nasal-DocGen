bc3d23c2c84201e4786ec44d094663a199c8ec67
Patrick Winters <19wintersp@gmail.com>
2023-01-21 14:20:59 +0000

    feat: parse file inputs

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
+#include <ctype.h>
+#include <dirent.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #ifndef NAME
@@ -9,7 +13,12 @@
 #define VERSION "0.1.0"
 
 extern char* optarg;
-extern int optopt;
+extern int optind, optopt;
+
+struct input {
+	const char* file;
+	const char* module;
+};
 
 int main(int argc, char* const argv[]) {
 	const char* output = NULL;
@@ -91,7 +100,107 @@ int main(int argc, char* const argv[]) {
 	if (output[0] == '=') output += 1;
 	if (template[0] == '=') template += 1;
 
+	struct input inputs[1024] = {};
+	int n_inputs = argc - optind;
+	bool search_dir = n_inputs == 0;
+
+	for (int i = optind; i < argc; i++) {
+		const char* module = strrchr(argv[i], ':');
+
+		if (module == NULL) {
+			inputs[i - optind] = (struct input) {
+				.file = argv[i],
+				.module = NULL
+			};
+		} else {
+			char* file_clone = malloc(module - argv[i] + 1);
+			strncpy(file_clone, argv[i], module - argv[i]);
+			file_clone[module - argv[i]] = 0;
+
+			size_t module_len = argv[i] + strlen(argv[i]) - module - 1;
+			char* module_clone = malloc(module_len + 1);
+			strncpy(module_clone, module + 1, module_len);
+			module_clone[module_len] = 0;
+
+			inputs[i - optind] = (struct input) {
+				.file = file_clone,
+				.module = module_clone + (module_clone[0] == '.' ? 1 : 0)
+			};
+
+			bool last_was_dot = false;
+			for (int j = 1; j < module_len + 1; j++) {
+				if (module[j] == '.') {
+					if (last_was_dot || j == module_len) {
+						fprintf(stderr, "%s: '%s' is invalid\n", argv[0], module + 1);
+						return 1;
+					}
+
+					last_was_dot = true;
+				} else {
+					if (!isalnum(module[j]) && module[j] != '_') {
+						fprintf(stderr, "%s: '%s' is invalid\n", argv[0], module + 1);
+						return 1;
+					}
+
+					last_was_dot = false;
+				}
+			}
+		}
+	}
+
+	if (search_dir) {
+		void search_level(struct input inputs[1024], int* n_inputs);
+
+		search_level(inputs, &n_inputs);
+	}
+
+	for (int i = 0; i < n_inputs; i++) {
+		if (inputs[i].module == NULL) {
+			// set default name
+		}
+	}
+
 	// todo
 
 	return 0;
+}
+
+char buf[4096] = { '.', '/', 0 };
+int buflen = 2;
+
+void search_level(struct input inputs[1024], int* n_inputs) {
+	DIR* level = opendir(buf);
+	if (level == NULL) return;
+
+	struct dirent* level_item;
+	while ((level_item = readdir(level))) {
+		if (strcmp(level_item->d_name, ".") == 0) continue;
+		if (strcmp(level_item->d_name, "..") == 0) continue;
+
+		strcpy(buf + buflen, level_item->d_name);
+
+		int buflen_before = buflen;
+		buflen += strlen(level_item->d_name);
+
+		if (level_item->d_type == DT_DIR) {
+			buf[buflen] = '/';
+			buflen += 1;
+			buf[buflen] = 0;
+
+			search_level(inputs, n_inputs);
+		} else if (strcmp(buf + buflen - 4, ".nas") == 0) {
+			char* buf_clone = malloc(buflen - 1);
+			strcpy(buf_clone, buf + 2);
+
+			inputs[(*n_inputs)++] = (struct input) {
+				.file = buf_clone,
+				.module = NULL
+			};
+		}
+
+		buf[buflen_before] = 0;
+		buflen = buflen_before;
+	}
+
+	closedir(level);
 }
