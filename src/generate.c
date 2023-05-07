358b506fea4a3978215fa77052b6efd8604f8d14
Patrick Winters <19wintersp@gmail.com>
2023-05-07 03:50:12 +0100

    feat: invoke item template

--- src/generate.c
+++ src/generate.c
@@ -259,3 +259,143 @@ static int document_module(struct ctx ctx, struct module* module) {
 
 	return 0;
 }
+
+static cJSON* item_to_json(struct item* item, struct list* stack) {
+	char crumbs[list_length(stack) * 3 + 1];
+
+	crumbs[0] = 0;
+	for (int i = 0; i < list_length(stack); i++) strcpy(crumbs + i * 3, "../");
+
+	cJSON* root = cJSON_CreateObject();
+
+	cJSON_AddStringToObject(root, "name", item->name);
+	cJSON_AddStringToObject(root, "root", crumbs);
+
+	static const char* types[] = { "var", "func", "class" };
+	cJSON_AddStringToObject(root, "type", types[item->type]);
+
+	if (item->desc != NULL) {
+		char* desc = cmark_markdown_to_html(item->desc, strlen(item->desc), 0);
+		cJSON_AddStringToObject(root, "desc", desc);
+		free(desc);
+	} else {
+		cJSON_AddStringToObject(root, "desc", "");
+	}
+
+	cJSON* parents = cJSON_AddArrayToObject(root, "parents");
+	LIST_ITER_T(stack, ancestor, const char*)
+		cJSON_AddItemToArray(parents, cJSON_CreateString(ancestor));
+
+	if (item->filename) {
+		cJSON* source = cJSON_AddObjectToObject(root, "source");
+
+		cJSON_AddStringToObject(source, "file", item->filename);
+		cJSON_AddNumberToObject(source, "line", (double) item->line);
+	} else {
+		cJSON_AddNullToObject(root, "source");
+	}
+
+	if (item->type == ITEM_FUNC) {
+		cJSON *params = cJSON_AddArrayToObject(root, "params");
+
+		LIST_ITER_T(item->items, param, struct param*) {
+			cJSON *nd = cJSON_CreateObject();
+
+			cJSON_AddStringToObject(nd, "name", param->name);
+			cJSON_AddBoolToObject(nd, "optional", param->optional);
+			cJSON_AddBoolToObject(nd, "variable", param->variable);
+
+			cJSON_AddItemToArray(params, nd);
+		}
+	}
+
+	if (item->type == ITEM_CLASS) {
+		cJSON* items[3] = {
+			cJSON_AddArrayToObject(root, "vars"),
+			cJSON_AddArrayToObject(root, "funcs"),
+			cJSON_AddArrayToObject(root, "classes"),
+		};
+
+		LIST_ITER_T(item->items, child, struct item*) {
+			cJSON* nd = cJSON_CreateObject();
+
+			cJSON_AddStringToObject(nd, "name", child->name);
+			cJSON_AddStringToObject(nd, "desc", child->desc);
+
+			cJSON_AddItemToArray(items[child->type], nd);
+		}
+	}
+
+	return root;
+}
+
+static int document_item(struct ctx ctx, struct item* item) {
+	DIR* dir = ctx.output;
+
+	if (item->type == ITEM_CLASS) {
+		if (mkdirat(dirfd(dir), item->name, DIR_FLAGS) == -1) {
+			if (errno != EEXIST) {
+				perrorf("failed to create output dir");
+				return 2;
+			}
+		}
+
+		int fd = openat(dirfd(dir), item->name, O_DIRECTORY);
+		ctx.output = fdopendir(fd);
+	}
+
+	char filename[11 + strlen(item->name)];
+	if (item->type == ITEM_CLASS) {
+		strcpy(filename, "index.html");
+	} else {
+		strcpy(filename, item->type == ITEM_VAR ? "var." : "func.");
+		strcat(filename, item->name);
+		strcat(filename, ".html");
+	}
+
+	int fd = openat(
+		dirfd(ctx.output), filename, O_CREAT | O_WRONLY | O_TRUNC, FILE_FLAGS
+	);
+	if (fd == -1) {
+		perrorf("failed to open output");
+		return 2;
+	}
+
+	FILE *file = fdopen(fd, "w");
+	if (!file) {
+		perrorf("failed to open output");
+		return 2;
+	}
+
+	cJSON* json = item_to_json(item, ctx.stack);
+
+	lattice_error *err = NULL;
+	const char *search[] = { ctx.templates.dir, NULL };
+	lattice_opts opts = { .search = search, .ignore_emit_zero = true };
+	lattice_cjson_file(ctx.templates.item, json, file, opts, &err);
+
+	fclose(file);
+	cJSON_Delete(json);
+
+	if (err) {
+		perrorf("failed to render item template (%s)", err->message);
+
+		lattice_error_code code = err->code;
+		lattice_error_free(err);
+		return code == LATTICE_IO_ERROR ? 2 : 3;
+	}
+
+	if (item->type == ITEM_CLASS) {
+		list_push(ctx.stack, item->name);
+
+		LIST_ITER_T(item->items, child, struct item *) {
+			int ret = document_item(ctx, child);
+			if (ret > 0) return ret;
+		}
+
+		ctx.output = dir;
+		list_pop(ctx.stack);
+	}
+
+	return 0;
+}
